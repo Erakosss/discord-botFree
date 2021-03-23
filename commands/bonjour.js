5bc1186cf67d1861de9742fe056c12887718c411
Erakosss <80986355+Erakosss@users.noreply.github.com>
2021-03-23 01:07:07 +0100

    TEST1

---
+++ commands/bonjour.js
@@ -0,0 +1,4 @@
+module.exports = {
+    run: message => message.channel.send('***Les moyens de paiement acceptés sont: Paypal, BTC, Revolut, Lydia***\n**• __Paypal__:** *https://www.paypal.com/paypalme/erakos* *(__en "Proches et Famille__)*\n**• __BTC__:** *1KpGyuEhv8ppqyWEZ6pNZTxurWh5YRfA8n*\n**• __Lydia__:** *https://lydia-app.com/collect/84002-uber/fr*\n**• __Revolut__:** *https://pay.revolut.com/baptisc62t*'),
+    name: 'paiement'
+}
